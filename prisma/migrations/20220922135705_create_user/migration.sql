b664933deae70f67a63e826232af98b55c700071
Rafael Costa <rafaelcosta8020@gmail.com>
2022-09-22 11:03:13 -0300

    update user schema

--- prisma/migrations/20220922135705_create_user/migration.sql
+++ prisma/migrations/20220922135705_create_user/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
