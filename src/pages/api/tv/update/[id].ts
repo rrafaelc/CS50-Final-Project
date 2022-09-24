ad3cd62290b11cbea8304867c0b9e750b1739e89
Rafael Costa <rafaelcosta8020@gmail.com>
2022-09-24 12:20:03 -0300

    api update by id

---
+++ src/pages/api/tv/update/[id].ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth/next'
+import { authOptions } from 'pages/api/auth/[...nextauth]'
+import { prisma } from 'lib/prisma'
+
+export default async function createTV(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  if (!session) {
+    // return res.status(401).send('Unauthorized')
+  }
+
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ message: `Method ${req.method} not allowed` })
+  }
+
+  const { status, season, episode } = req.body
+  const id = String(req.query.id)
+
+  // If is not number
+  !Number(season) && res.status(400).json({ message: 'season must be number' })
+  !Number(episode) &&
+    res.status(400).json({ message: 'episode must be number' })
+
+  try {
+    await prisma.tvShow.update({
+      where: {
+        id,
+      },
+      data: {
+        season,
+        episode,
+        status,
+      },
+    })
+
+    return res.status(204).send({})
+  } catch (err: any) {
+    console.log(err)
+  }
+
+  return res.status(400).send('Tv Show not found')
+}
