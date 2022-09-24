f8e37182ba95afb1fb0f2aed41743b2a792e2957
Rafael Costa <rafaelcosta8020@gmail.com>
2022-09-24 12:19:36 -0300

    api create

--- src/pages/api/tv/create.ts
+++ src/pages/api/tv/create.ts
@@ -1,14 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from 'lib/prisma'
+import { unstable_getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
 
 export default async function createTV(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { userId, tvId, title, genre, status, season, episode } = req.body
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  // If not session
+  if (!session) {
+    // return res.status(401).send('Unauthorized')
+  }
+
+  // const userId = session.user.id
+  const userId = process.env.USER_ID
+  const { tvApiId, title, genre, status, season, episode, poster } = req.body
 
   // If is not number
-  !Number(tvId) && res.status(400).json({ message: 'tvID must be number' })
+  !Number(tvApiId) &&
+    res.status(400).json({ message: 'tvApiId must be number' })
   !Number(season) && res.status(400).json({ message: 'season must be number' })
   !Number(episode) &&
     res.status(400).json({ message: 'episode must be number' })
@@ -21,7 +33,7 @@ export default async function createTV(
   const tv = await prisma.tvShow.findFirst({
     where: {
       userId,
-      tvId,
+      tvApiId,
     },
   })
 
@@ -34,12 +46,13 @@ export default async function createTV(
   await prisma.tvShow.create({
     data: {
       userId,
-      tvId,
+      tvApiId,
       title,
       genre,
       status,
       season,
       episode,
+      poster,
     },
   })
 
