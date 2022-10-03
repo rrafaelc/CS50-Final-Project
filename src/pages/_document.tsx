a722d389c13ebe29e02ea020041e61e623961be8
Rafael Costa <rafaelcosta8020@gmail.com>
2022-10-03 10:01:01 -0300

    Change title and add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,25 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Roboto:wght@400;500&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <title>CS50 Final Project</title>
         </Head>
         <body>
           <Main />
